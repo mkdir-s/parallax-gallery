8bd629b2162784e69bcba2bc252675f7d39dfe2c
ihtml <i.html06@gmail.com>
2023-06-10 13:08:31 +0600

    init: initial project

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const sliderMain = new Swiper('.slider_main', {
+  freeMode: true,
+  centeredSlides: true,
+  slidesPerView: 3.5,
+  mousewheel: true,
+  parallax: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2.5,
+      spaceBetween: 20
+    },
+    680: {
+      slidesPerView: 3.5,
+      spaceBetween: 60
+    }
+  }
+})
+
+const sliderBg = new Swiper('.slider_bg', {
+  centeredSlides: true,
+  parallax: true,
+  spaceBetween: 60,
+  slidesPerView: 3.5
+})
+sliderMain.controller.control = sliderBg;
+
+document.querySelectorAll('.slider__item').forEach(item => {
+  item.addEventListener('click', event => {
+    item.classList.toggle('opened');
+  })
+})
+
+let descr = document.querySelector('.descr');
+sliderMain.on('slideChange', event => {
+  sliderMain.activeIndex > 0 ? descr.classList.add('hidden') : descr.classList.remove('hidden');
+})
